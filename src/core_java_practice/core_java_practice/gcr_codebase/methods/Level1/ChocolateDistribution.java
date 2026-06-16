d8f3a0d529ccce6dfd6f12316a0c6941f59221fd
Shivam Shukla <shuklashivam63063@gmail.com>
2026-06-16 09:07:17 +0530

    [Shivam] Add: Level-1 Methods

---
+++ src/core_java_practice/core_java_practice/gcr_codebase/methods/Level1/ChocolateDistribution.java
@@ -0,0 +1,28 @@
+package core_java_practice.core_java_practice.gcr_codebase.methods.Level1;
+
+import java.util.Scanner;
+
+public class ChocolateDistribution {
+
+    public static int[] findRemainderAndQuotient(int chocolates, int children) {
+        int eachChildGets = chocolates / children;
+        int remaining = chocolates % children;
+
+        return new int[]{eachChildGets, remaining};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of chocolates: ");
+        int chocolates = sc.nextInt();
+
+        System.out.print("Enter number of children: ");
+        int children = sc.nextInt();
+
+        int[] result = findRemainderAndQuotient(chocolates, children);
+
+        System.out.println("Each child gets = " + result[0]);
+        System.out.println("Remaining chocolates = " + result[1]);
+    }
+}
---
+++ src/core_java_practice/core_java_practice/gcr_codebase/methods/Level1/SumNaturalNumbers.java
@@ -0,0 +1,25 @@
+package core_java_practice.core_java_practice.gcr_codebase.methods.Level1;
+
+import java.util.Scanner;
+
+public class SumNaturalNumbers {
+
+    public static int findSum(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        System.out.println("Sum = " + findSum(n));
+    }
+}
