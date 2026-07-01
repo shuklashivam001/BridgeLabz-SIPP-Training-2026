8a19158e2cd92bbc3b1ba8b9e359d05dc2e88fb0
Shivam Shukla <shuklashivam63063@gmail.com>
2026-07-01 09:19:45 +0530

    [Shivam] Add: Day14 Problem

---
+++ src/oops_practice/oops_practice/gcr_codebase/interfaces/story_based/FitnessTracker.java
@@ -0,0 +1,46 @@
+package oops_practice.oops_practice.gcr_codebase.interfaces.story_based;
+
+interface Trackable {
+
+    void logActivity();
+
+    default void resetData() {
+        System.out.println("Fitness data has been reset.");
+    }
+}
+
+interface Reportable {
+    void generateReport();
+}
+
+interface Notifiable {
+    void sendAlert();
+}
+
+class FitnessDevice implements Trackable, Reportable, Notifiable {
+
+    public void logActivity() {
+        System.out.println("Activity logged successfully.");
+    }
+
+    public void generateReport() {
+        System.out.println("Fitness report generated.");
+    }
+
+    public void sendAlert() {
+        System.out.println("Health alert sent to the user.");
+    }
+}
+
+public class FitnessTracker {
+
+    public static void main(String[] args) {
+
+        FitnessDevice device = new FitnessDevice();
+
+        device.logActivity();
+        device.generateReport();
+        device.sendAlert();
+        device.resetData();
+    }
+}
